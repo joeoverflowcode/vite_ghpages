3f19c84aa7c697d967a0a8fe0838b9ec47258d9c
Joe Aguado <aguado.joe@gmail.com>
2024-05-17 10:01:38 -0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/gh_ghpages/',
+  base: '/vite_ghpages/',
   plugins: [react()],
 })
